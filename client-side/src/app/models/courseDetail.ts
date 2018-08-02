e01932a31e29b4dd4140689b2c991fa6b75f54df
Tuan Nguyen <tuannguyen@tuans-mbp.attlocal.net>
2018-08-01 23:12:23 -0400

    Add prerequisite to form

--- client-side/src/app/models/courseDetail.ts
+++ client-side/src/app/models/courseDetail.ts
@@ -3,6 +3,7 @@ export class CourseDetail {
 		major: string; // cs (or CS)
 		courseNumber: string; // 1331
 		credit: string; // 3 and up
+		prerequisite: string; // Tricky one, can skip for now. (example: require (MATH1100 and MATH1101) or (MATH2010) => (MATH1100&&MATH1101)||(MATH2010))
 	}
 
 	// List of sections of the course (Ex: CS1331 have different section like A1 A2 A3 B1 B2)
@@ -18,7 +19,7 @@ export class CourseDetail {
 			[
 				{
 					time: string // MWF|11001200
-					location: string // Clough 152 (if possible)
+					location: string // Clough 152
 				}
 			];
 		}
