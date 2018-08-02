ce996e514a67452075052bbdd0e389b65a82d700
Tuan Nguyen <tuannguyen@tuans-mbp.attlocal.net>
2018-08-01 23:02:14 -0400

    Course's required fields

---
+++ client-side/src/app/models/courseDetail.ts
@@ -0,0 +1,26 @@
+export class CourseDetail {
+	generalInfo: {
+		major: string; // cs (or CS)
+		courseNumber: string; // 1331
+		credit: string; // 3 and up
+	}
+
+	// List of sections of the course (Ex: CS1331 have different section like A1 A2 A3 B1 B2)
+	sectionInfo: 
+	[
+		{
+			sectionName: string; // A1;
+			professor: string;
+			crn: string; // 12345
+
+			// Some classes have more than 1 location and time (like CS 2050)
+			classInfo: 
+			[
+				{
+					time: string // MWF|11001200
+					location: string // Clough 152 (if possible)
+				}
+			];
+		}
+	];
+}
