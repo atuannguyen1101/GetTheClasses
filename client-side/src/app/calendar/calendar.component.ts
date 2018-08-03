1efcb46246ce536798d58a449b2d0500460b6ea6
Tuan Nguyen <tuannguyen@lawn-128-61-119-79.lawn.gatech.edu>
2018-08-02 22:08:43 -0400

    Draggable events in Calendar

--- client-side/src/app/calendar/calendar.component.ts
+++ client-side/src/app/calendar/calendar.component.ts
@@ -25,12 +25,59 @@ export class CalendarComponent implements OnInit {
   		editable: true,
   		unselectAuto: false,
   		snapDuration: '00:05:00',
-  		select: function (start, end, jsEvent, view) {
-		    console.log(moment(start).format('HH:mm'));
-		    console.log(moment(end).format('HH:mm'));
-		    console.log(jsEvent.target);
-		}
-  	});
-  }
+  		nowIndicator: false,
+			select: function (start, end, jsEvent, view) {
+			    $("#calendar").fullCalendar('addEventSource', [{
+			        start: start,
+			        end: end,
+			        // rendering: 'background',
+			        block: true,
+			    }]);
+			},
+			selectOverlap: function(event) {
+			    return ! event.block;
+			},
+			eventRender: function(event, element, view) {
+        if (view.name == 'listDay') {
+            element.find(".fc-list-item-time").append("<span class='closeon'>X</span>");
+        } else {
+            element.find(".fc-content").prepend("<span class='closeon'>X</span>");
+        }
+        element.find(".closeon").on('click', function() {
+          $('#calendar').fullCalendar('removeEvents',event._id);
+          console.log('delete');
+        });
+	    }  	
+	  });
 
+  	$("#button").click(function() {
+  		$('#calendar').fullCalendar('refetchEvents');
+  		var events = $('#calendar').fullCalendar('clientEvents');
+  		var stringTime = [];
+  		var dictTime = {
+  			m: [],
+  			t: [],
+  			w: [],
+  			r: [],
+  			f: [],
+  			s: []
+  		};
+  		for (var e of events) {
+  			if ((moment(e.start).format('dd')) == "Th") {
+  				var period = "r" + moment(e.start).format("HHmm") + moment(e.end).format("HHmm");
+  			}
+  			else {
+  				var period = ("" + moment(e.start).format("dd")[0]).toLowerCase() + moment(e.start).format("HHmm") + moment(e.end).format("HHmm");
+  			}
+  			stringTime.push(period);
+  		}
+  		stringTime.sort();
+  		for (var time of stringTime) {
+  			dictTime[time[0]].push(
+  				[parseInt(time.substring(1,5)), parseInt(time.substring(5, time.length))]
+  				);
+  		}
+  		console.log(dictTime)
+  	})
+  }
 }
