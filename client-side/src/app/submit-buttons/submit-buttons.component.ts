ff2b698e707d595646479233e1d3d6dca78e806d
Tuan Nguyen <tuannguyen@lawn-128-61-119-79.lawn.gatech.edu>
2018-08-08 11:34:31 -0400

    Testing Git on Sublime

---
+++ client-side/src/app/submit-buttons/submit-buttons.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { TransferDataService } from '../services/transfer-data.service';
+
+@Component({
+  selector: 'app-submit-buttons',
+  templateUrl: './submit-buttons.component.html',
+  styleUrls: ['./submit-buttons.component.css']
+})
+export class SubmitButtonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
