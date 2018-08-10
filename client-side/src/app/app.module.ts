ee46af513dfe36a7e4bec1c42e8bcbda29f4c091
Thinh Nguyen <tnntech@gatech.edu>
2018-08-09 22:28:15 -0400

    calendar add

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -5,6 +5,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { InputComponent } from './input/input.component';
 import { CalendarComponent } from './calendar/calendar.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import {DropdownModule} from 'primeng/primeng';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +20,12 @@ import { CalendarComponent } from './calendar/calendar.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AutoCompleteModule,
+    BrowserAnimationsModule,
+    DropdownModule,
+    NgSelectModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
