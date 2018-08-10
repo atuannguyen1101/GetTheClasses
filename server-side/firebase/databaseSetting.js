5adff785f55f96ba8ea2adbf9e9b31fb1fed8977
Tuan Nguyen <atuannguyen1101@gmail.com>
2018-08-10 18:02:57 -0400

    Optimize update data (just update the changed field). Add Create new Database. Ignore test files.

--- server-side/firebase/databaseSetting.js
+++ server-side/firebase/databaseSetting.js
@@ -4,11 +4,20 @@ async function run() {
 	while (true) {
 	    console.log("\n\n\n************\nServer-side options: ");
 	    console.log("1. Update database");
+	    console.log("2. Create new database");
 	    var option = read.question("\nOption: ");
 	    switch(parseInt(option)) {
 	        case 1: {
 	            await require('./updateDatabase.js').run();
 	            break;
+	        };
+	        case 2: {
+	        	await require('./createNewDatabase.js').run();
+	        	break;
+	        };
+	        default: {
+	        	console.log("Nothing to do!");
+	        	break;
 	        }
 	    }
 	}
