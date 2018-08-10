3367c15a1e9505b4835f6fc06ec36ea55030e103
Thinh Nguyen <tnntech@gatech.edu>
2018-08-10 13:49:59 -0400

    bring button into allday field

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // import { MatCheckboxModule } from '@angular/material/checkbox';
-// import { MatButtonModule } from '@angular/material/button';
-
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
 import { InputComponent } from './input/input.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AutoCompleteModule } from 'primeng/autocomplete';
-import {DropdownModule} from 'primeng/primeng';
+import { DropdownModule } from 'primeng/primeng';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { FormsModule } from '@angular/forms';
 import { SubmitButtonsComponent } from './submit-buttons/submit-buttons.component';
@@ -29,7 +27,8 @@ import { SubmitButtonsComponent } from './submit-buttons/submit-buttons.componen
     BrowserAnimationsModule,
     DropdownModule,
     NgSelectModule,
-    FormsModule
+    FormsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client-side/src/app/calendar/calendar.component.ts
+++ client-side/src/app/calendar/calendar.component.ts
@@ -20,104 +20,140 @@ export class CalendarComponent implements OnInit {
 		eventsData: any[] = [];
 		timeRanges = {};
 
-  ngOnInit() {
-  	$('#calendar').fullCalendar({
-  		header: false,
-  		defaultView: "agendaWeek",
-  		hiddenDays: [0],
-  		columnHeaderFormat: "dddd",
-  		minTime: '08:00:00',
-  		maxTime: '21:00:00',
-  		selectable: true,
-  		selectHelper: true,
-  		editable: true,
-  		unselectAuto: false,
-  		snapDuration: '00:05:00',
-  		contentHeight: "auto",
-  		eventOverlap: false,
-  		// eventColor: "red",
-  		eventResize: (event) => {
+	calendarCompo() {
+		$('#calendar').fullCalendar({
+			header: false,
+			defaultView: "agendaWeek",
+			hiddenDays: [0],
+			columnHeaderFormat: "dddd",
+			minTime: '08:00:00',
+			maxTime: '21:00:00',
+			selectable: true,
+			selectHelper: true,
+			editable: true,
+			unselectAuto: false,
+			snapDuration: '00:05:00',
+			contentHeight: "auto",
+			eventOverlap: false,
+			// defaultDate: '2018-08-20',
+			// eventColor: "red",
+			eventResize: (event) => {
+				// Handle resize issue here (start-end range is too small)
+				console.log(event.start.format("HH:mm"));
+				console.log(event.end.format("HH:mm"));
+			},
+			selectConstraint: {
+				start: "08:00",
+				end: "21:00"
+			},
+		dayClick: (date, jsEvent, view) => {
+		  if ($(jsEvent.target).hasClass('fc-day')) {
+			var freeTime = this.transferDataService.getFreeTime();
+			if (freeTime == undefined || freeTime[this.dictTime[date.day()]] == []
+			  || freeTime[this.dictTime[date.day()]].toString() != [[800, 2100]].toString()) {
+			  for (var e of $('#calendar').fullCalendar('clientEvents')) {
+					if (e.start.day() == date.day()) {
+						$('#calendar').fullCalendar('removeEvents', e._id);
+					}
+			  }
 
-  			// Handle resize issue here (start-end range is too small)
-  			console.log(event.start.format("HH:mm"));
-  			console.log(event.end.format("HH:mm"));
-  		},
-  		selectConstraint: {
-  			start: "08:00",
-  			end: "21:00"
-  		},
-      dayClick: (date, jsEvent, view) => {
-        if ($(jsEvent.target).hasClass('fc-day')) {
-          var freeTime = this.transferDataService.getFreeTime();
-          if (freeTime == undefined || freeTime[this.dictTime[date.day()]] == []
-            || freeTime[this.dictTime[date.day()]].toString() != [[800, 2100]].toString()) {
+			  var start = moment("08:00", "hh:mm");
+				start.day(date.day());
+				console.log(start);
+			  var end = moment("21:00", "hh:mm");
+				end.day(date.day());
 
-            for (var e of $('#calendar').fullCalendar('clientEvents')) {
-              if (e.start.day() == date.day()) {
-                $('#calendar').fullCalendar('removeEvents', e._id);
-              }
-            }
-
-            var start = moment("08:00", "hh:mm");
-            start.day(date.day());
-            var end = moment("21:00", "hh:mm");
-            end.day(date.day());
-
-            $('#calendar').fullCalendar('renderEvent', {
-              start: start,
-              end: end
-            })
-          }
-          else {
-            for (var e of $('#calendar').fullCalendar('clientEvents')) {
-              if (e.start.day() == date.day()) {
-                $('#calendar').fullCalendar('removeEvents', e._id);
-                var events = $('#calendar').fullCalendar('clientEvents');
-                var freeTimeDict = this.calendarHelperService.scheduleTimeFormat(events);
-                this.transferDataService.setFreeTime(freeTimeDict);
-              }
-            }
-          }
-        }
-      },
-			select: (start, end) => {
-				if (!this.calendarHelperService.isValidTime(start, end)) {
-					end = start.clone().add(45, 'm');
-          for (var e of $('#calendar').fullCalendar('clientEvents')) {
-            if (start < e.start && end > e.start ) {
-              $('#calendar').fullCalendar('unselect');
-              alert("Not enough space");
-              return ;
-            }
-          }
+			  $('#calendar').fullCalendar('renderEvent', {
+				start: start,
+				end: end
+			  })
+			}
+			else {
+			  for (var e of $('#calendar').fullCalendar('clientEvents')) {
+					if (e.start.day() == date.day()) {
+						$('#calendar').fullCalendar('removeEvents', e._id);
+						var events = $('#calendar').fullCalendar('clientEvents');
+						var freeTimeDict = this.calendarHelperService.scheduleTimeFormat(events);
+						this.transferDataService.setFreeTime(freeTimeDict);
+					}
+			  }
+			}
+		  }
+		},
+		select: (start, end) => {
+			if (!this.calendarHelperService.isValidTime(start, end)) {
+				end = start.clone().add(45, 'm');
+				for (var e of $('#calendar').fullCalendar('clientEvents')) {
+					if (start < e.start && end > e.start ) {
+						$('#calendar').fullCalendar('unselect');
+						alert("Not enough space");
+						return;
+					}
 				}
-				$('#calendar').fullCalendar('unselect');
-		    $("#calendar").fullCalendar('addEventSource', [{
-		        start: start,
-		        end: end,
-		        block: true
-		    }]);
-			},
-			selectOverlap: function(event) {
-		    return ! event.block;
-			},
-			eventRender: (event, element, view) => {
-        if (view.name == 'listDay') {
-            element.find(".fc-list-item-time").append("<button class='closeon'>X</button>")
-        } else {
-            element.find(".fc-content").prepend("<button class='closeon'>X</button>")
-        }
-        element.find(".closeon").on('click', () => {
-          $('#calendar').fullCalendar('removeEvents',event._id);
-          var events = $('#calendar').fullCalendar('clientEvents');
-          var freeTimeDict = this.calendarHelperService.scheduleTimeFormat(events);
-          this.transferDataService.setFreeTime(freeTimeDict);
-        });
-  			var events = $('#calendar').fullCalendar('clientEvents');
-        var freeTimeDict = this.calendarHelperService.scheduleTimeFormat(events);
-        this.transferDataService.setFreeTime(freeTimeDict);
-	    },
-	  });
+			}
+			$('#calendar').fullCalendar('unselect');
+			// console.log(start);
+			// console.log(end);
+			$("#calendar").fullCalendar('addEventSource', [{
+				start: start,
+				end: end,
+				block: true
+			}]);
+		},
+		selectOverlap: function(event) {
+			return ! event.block;
+		},
+		eventRender: (event, element, view) => {
+			// element.find(".fc-mon").prepend("<button class='closeon'>X</button>")
+			// element.find("fc-day fc-widget-content fc-mon fc-past").append("<button class='closeon'>X</button>")
+			if (view.name == 'listDay') {
+				element.find(".fc-list-item-time").append("<button class='closeon'>X</button>")
+			} else {
+				element.find(".fc-content").prepend("<button class='closeon'>X</button>")
+			}
+			element.find(".closeon").on('click', () => {
+				$('#calendar').fullCalendar('removeEvents',event._id);
+				var events = $('#calendar').fullCalendar('clientEvents');
+				var freeTimeDict = this.calendarHelperService.scheduleTimeFormat(events);
+				this.transferDataService.setFreeTime(freeTimeDict);
+			});
+			var events = $('#calendar').fullCalendar('clientEvents');
+			var freeTimeDict = this.calendarHelperService.scheduleTimeFormat(events);
+			this.transferDataService.setFreeTime(freeTimeDict);
+		},
+		viewRender: (view, element) => {
+			if (view.name == 'agendaWeek') {
+				for (var i = 0; i < 6; i++) {
+					var date = '';
+					switch (i) {
+						case 0:
+							date = '.fc-mon';
+							break
+						case 1:
+							date = '.fc-tue';
+							break
+						case 2:
+							date = '.fc-wed';
+							break
+						case 3:
+							date = '.fc-thu';
+							break
+						case 4:
+							date = '.fc-fri';
+							break
+						case 5:
+							date ='.fc-sat';
+							break
+					}
+					$(".fc-body " + ".fc-row " + date).prepend("<div align='center'><button class='timeRangeStyle' mat-button>Select Time</button></div>")
+				}
+			}
+		}
+	});
+	}
+
+  ngOnInit() {
+	  this.calendarCompo();
   	}
 
 	recieveMess($event) {
@@ -136,8 +172,10 @@ export class CalendarComponent implements OnInit {
 		for (var i = 0; i < this.timeRanges['start'].length; i++) {
 			this.eventsData.push({
 				title: $event.name + '  Professor: ' + $event.professor,
-				start: this.timeRanges['start'][i],
-				end: this.timeRanges['end'][i],
+				// start: this.timeRanges['start'][i],
+				// end: this.timeRanges['end'][i],
+				start: moment("08:00", "hh:mm").day("Monday"),
+				end: moment("12:00", "hh:mm").day("Monday"),
 				borderColor: 'black',
 				textColor: 'white',
 				color: randomColor,
@@ -151,8 +189,13 @@ export class CalendarComponent implements OnInit {
 		}
 	}
 
+	// var start = moment("08:00", "hh:mm");
+	// start.day(date.day());
+	// var end = moment("21:00", "hh:mm");
+	// end.day(date.day());
+
 	getRandomColor() {
-		var letters = '0123456789ABCDEF';
+		var letters = '0123456789ABCDEF'; //HEX
 		var color = '#';
 		for (var i = 0; i < 6; i++) {
 		color += letters[Math.floor(Math.random() * 16)];
@@ -162,31 +205,32 @@ export class CalendarComponent implements OnInit {
 
 	analyzetimeStart(str) {
 		var time = str.slice(str.indexOf('-') + 1, str.indexOf('-') + 7);
-		return time.trim() + ':00'
+		return time.trim()
 	}
 
 	analyzetimeEnd(str) {
 		var time = str.slice(str.length - 6, str.length);
-		return time.trim() + ':00'
+		return time.trim()
 	}
 
 
 	analyzeDate(str) {
 		var date = {
-			'M': '2018-08-20',
-			'T': '2018-08-21',
-			'W': '2018-08-22',
-			'Th': '2018-08-23',
-			'F': '2018-08-24'
+			'M': 'Monday',
+			'T': 'Tuesday',
+			'W': 'Wednesday',
+			'Th': 'Thursday',
+			'F': 'Friday',
+			'Sa': 'Sartuday'
 		}
 		var timeRange = str.slice(0, str.indexOf('-') - 1).trim();
 		for (var i = 0; i < timeRange.length; i++) {
 			var currDate = timeRange[i];
 			var dateFormat = date[currDate];
-			var timeStart = this.analyzetimeStart(str);
-			var timeEnd = this.analyzetimeEnd(str);
-			this.timeRanges['start'].push(dateFormat + 'T' + timeStart);
-			this.timeRanges['end'].push(dateFormat + 'T' + timeEnd)
+			// var timeStart = this.analyzetimeStart(str);
+			// var timeEnd = this.analyzetimeEnd(str);
+			this.timeRanges['start'].push(dateFormat);
+			this.timeRanges['end'].push(dateFormat)
 		}
 	}
 }
