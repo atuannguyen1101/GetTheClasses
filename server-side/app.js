d9178ab8fac84a204f9f524bcdbdac3dd4695bb2
Tuan Nguyen <tuannguyen@tuans-mbp.attlocal.net>
2018-08-09 20:25:56 -0400

    Update whole database. Compare old and new files before update

--- server-side/app.js
+++ server-side/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
-const main = require('./scripts/getClasses')
+const main = require('./scripts/getClasses');
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -15,6 +15,7 @@ app.route('/api/course').post((req, res) => {
     console.log("Recieved call to api/course")
     if (req.body.freeTime == undefined) 
         req.body.freeTime = null;
+
     main(req.body.criteria, req.body.freeTime).then((data) => {
         if (data.length > 0 && data[0].length > 0) {
             res.send({
