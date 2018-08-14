fa0ba30c786de70f42e5aa345424aef55cd99440
Thinh Nguyen <tnntech@gatech.edu>
2018-08-14 02:36:29 -0400

    icon image file to use it later if need

--- client-side/src/app/display-data/display-data.component.ts
+++ client-side/src/app/display-data/display-data.component.ts
@@ -13,4 +13,15 @@ export class DisplayDataComponent implements OnInit {
 
   ngOnInit() {}
 
+  mouseEnter(div : string){
+    console.log("mouse enter : " + div);
+  }
+
+  mouseLeave(div : string){
+    console.log('mouse leave :' + div);
+  }
+
+  test(event) {
+    console.log(event);
+  }
 }
